2a9c1c932d2167149397596433b07468fadecb2e
Ug0Security <c.vallicari@gmail.com>
2023-06-12 09:53:09 +0200

    Update txt2gif.sh

--- txt2gif.sh
+++ txt2gif.sh
@@ -2,9 +2,10 @@ echo "Prompt : $2"
 echo " Crafting imgs.."
 proxychains -q bash txt2imggif.sh "$1" "$2" &
 proxychains -q bash txt2imggif.sh "$1" "$2" &
-proxychains -q bash txt2imggif.sh "$1" "$2" 
+proxychains -q bash txt2imggif.sh "$1" "$2" &
+wait
 echo "Crafting GIF.."
+convert -limit memory 3MB -delay 100 -loop 0 *.png test.gif
 sleep 3
-convert -limit memory 3MB -delay 100 -loop 0 test*.png test.gif
 rm test*.png
 display test.gif
