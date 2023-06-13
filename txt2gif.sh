5ea611a4dafe4e6dc52c4c91e33a60366c3cb185
Ug0Security <c.vallicari@gmail.com>
2023-06-13 17:58:05 +0200

    Update txt2gif.sh

--- txt2gif.sh
+++ txt2gif.sh
@@ -1,11 +1,14 @@
 echo "Prompt : $2"
 echo " Crafting imgs.."
-proxychains -q bash txt2imggif.sh "$1" "$2" &
-proxychains -q bash txt2imggif.sh "$1" "$2" &
-proxychains -q bash txt2imggif.sh "$1" "$2" &
+seed=$(shuf -i 1000000000-9999999999 -n 1)
+proxychains -q bash txt2imggif.sh "$1" "$2" "$seed"&
+proxychains -q bash txt2imggif.sh "$1" "$2" "$seed"&
+proxychains -q bash txt2imggif.sh "$1" "$2" "$seed"&
+proxychains -q bash txt2imggif.sh "$1" "$2" "$seed"&
+proxychains -q bash txt2imggif.sh "$1" "$2" "$seed"&
 wait
 echo "Crafting GIF.."
-convert -limit memory 3MB -delay 100 -loop 0 *.png test.gif
+convert -limit memory 3MB -delay 100 -loop 0 *.png test$seed.gif
 sleep 3
-rm test*.png
-display test.gif
+#rm test*.png
+display test$seed.gif
