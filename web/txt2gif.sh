c5be6d1764d025476afb11373633690b8535db1a
Ug0Security <c.vallicari@gmail.com>
2023-06-14 08:49:10 +0200

    Create txt2gif.sh

---
+++ web/txt2gif.sh
@@ -0,0 +1,26 @@
+echo "Prompt : $2"
+echo " Crafting imgs.."
+
+if [ "$3" == "fixed" ]
+then
+if [[ ! -z "$4" ]]
+then
+seed=$4
+else
+seed=$(shuf -i 1000000000-9999999999 -n 1)
+fi
+fi
+
+proxychains -q bash txt2imggif.sh "$1" "$2" "$seed"&
+proxychains -q bash txt2imggif.sh "$1" "$2" "$seed"&
+proxychains -q bash txt2imggif.sh "$1" "$2" "$seed"&
+proxychains -q bash txt2imggif.sh "$1" "$2" "$seed"&
+proxychains -q bash txt2imggif.sh "$1" "$2" "$seed"&
+wait
+echo "Crafting GIF.."
+convert -limit memory 3MB -delay 100 -loop 0 *.png ./gif/test$seed.gif
+sleep 1
+cp ./gif/test$seed.gif ./gif/test.gif
+sleep 3
+rm test*.png
+#display test.gif
