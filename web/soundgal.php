62230bcf4538f672f452817997f9af812a84ff03
Ug0Security <c.vallicari@gmail.com>
2023-06-25 12:37:46 +0200

    Create soundgal.php

---
+++ web/soundgal.php
@@ -0,0 +1,41 @@
+<?php
+   // images directory
+   $imagesDirectory = "sounds";
+
+   // list of files to skip
+   $toSkip = [".", "..", ".htaccess", ".htpasswd"];
+
+?>
+<section id="quickGallery" class="slideInLeft">
+   <h1>sound Gallery</h1>
+   
+      <?php 
+         $images = scandir($imagesDirectory);
+
+         if (empty(array_diff($images, $toSkip))) {
+            echo "<p>No sound in <b>$imagesDirectory</b> directory</p>";
+         } else {
+            foreach ($images as $fileName) 
+            {
+               if (in_array($fileName, $toSkip)) continue;
+               echo <<<_EOF
+               <div class="galleryImage">
+                  <video width="320" height="240" controls>
+		     <source src="$imagesDirectory/$fileName" type="video/mp4">
+                  </video>
+               </div>
+               <!-- /.galleryImage -->
+_EOF;
+            }
+         }
+      ?>
+   
+</section>
+<!-- /#quickGallery -->
+
+<div id="imagePreview" class="quickModal">
+   <div id="modalContent">
+      <span class="closeBtn"><i class="fa fa-times" aria-hidden="true"></i></span>
+      <img src="#" alt="">
+   </div>
+</div>
