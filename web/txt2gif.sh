9010d6e5162961270e904038a110a1d4ad730f4e
Ug0Security <c.vallicari@gmail.com>
2023-06-28 16:22:53 +0200

    Update txt2gif.sh

--- web/txt2gif.sh
+++ web/txt2gif.sh
@@ -17,10 +17,34 @@ proxychains -q bash txt2imggif.sh "$1" "$2" "$seed"&
 proxychains -q bash txt2imggif.sh "$1" "$2" "$seed"&
 proxychains -q bash txt2imggif.sh "$1" "$2" "$seed"&
 wait
+
 echo "Crafting GIF.."
 convert -limit memory 3MB -delay 500 -loop 0 *.png ./gif/test$seed.gif
 sleep 1
-cp ./gif/test$seed.gif ./gif/test.gif
+
+
+
+echo "Crafting Anim imgs.."
+
+proxychains -q bash imgtoanim.sh $(ls *.png | head -1 | tail +1) &
+proxychains -q bash imgtoanim.sh $(ls *.png | head -2 | tail +2) &
+proxychains -q bash imgtoanim.sh $(ls *.png | head -3 | tail +3) &
+proxychains -q bash imgtoanim.sh $(ls *.png | head -4 | tail +4) &
+proxychains -q bash imgtoanim.sh $(ls *.png | head -5 | tail +5) &
+wait
+
+sleep 5
+
+for i in $(ls *.mp4);
+do
+echo file $i >> list.txt
+done
+
+ffmpeg -y -hide_banner -loglevel error -f concat -safe 0 -i list.txt -c copy vids/output.mp4
+
 sleep 3
+rm anim*.json
 rm *.png
+rm *.mp4
+rm list.txt
 #display test.gif
