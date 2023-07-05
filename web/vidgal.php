8d1775b3d81469dc5e9214095339931777ec5615
Ug0Security <c.vallicari@gmail.com>
2023-07-05 17:12:11 +0200

    Update vidgal.php

--- web/vidgal.php
+++ web/vidgal.php
@@ -8,7 +8,9 @@
 ?>
 <section id="quickGallery" class="slideInLeft">
    <h1>Vids Gallery</h1>
-   
+      <style>
+body {background-color: black;}
+</style>
       <?php 
          $images = scandir($imagesDirectory);
 
