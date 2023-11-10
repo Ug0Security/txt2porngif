8c9e3148fc7bbada576311cd6ae064409024762e
Ug0Security <c.vallicari@gmail.com>
2023-11-10 12:05:54 +0100

    Update and rename hey.sh to img2deepmap.sh

--- img2deepmap.sh
+++ img2deepmap.sh
@@ -1,7 +1,7 @@
 #auth
 auth="Authorization: Bearer $(python log.py) "
 
-echo "get upload url"
+#echo "get upload url"
 
 test=$(curl -sk -H "$auth" "https://leiapixconverter-backend.leiapix.com/api/v2/generateTemporaryUploadPresignedUrl?mimeType=image%2Fjpeg")
 
@@ -9,21 +9,21 @@ url=$(echo $test | jq '.uploadUrl' | tr -d '"')
 fileid=$(echo $test | jq '.fileId' | tr -d '"')
 #echo  $url
 #echo  $fileid
-echo "upload file"
-curl -ski --upload-file /tmp/test.jpeg "$url" > /dev/null
+#echo "upload file"
+curl -ski --upload-file $1 "$url" > /dev/null
 
 
 
-echo "create map"
+#echo "create map"
 mapurl=$(curl -sk -X POST -H "$auth" -H "Content-Type: application/json" -d "{\"originalImageFileId\":\"$fileid\"}" "https://leiapixconverter-backend.leiapix.com/api/v2/generateDepthMap" |  jq '.downloadUrl' | tr -d '"')
 #echo  $mapurl
 
-echo "download map"
+#echo "download map"
 
-curl -sk $mapurl -o testmap.jpg
+curl -sk $mapurl -o testmap$2.jpg
 
 
-echo "get upload url map"
+#echo "get upload url map"
 
 test2=$(curl -sk -H "$auth" "https://leiapixconverter-backend.leiapix.com/api/v2/generateTemporaryUploadPresignedUrl?mimeType=image%2Fjpeg")
 
@@ -32,14 +32,18 @@ fileidmap=$(echo $test2 | jq '.fileId' | tr -d '"')
 #echo  $urlmap
 #echo  $fileidmap
 
-echo "upload map"
-curl -ski --upload-file /tmp/testmap.jpg "$urlmap" > /dev/null
+#echo "upload map"
+curl -ski --upload-file testmap$2.jpg "$urlmap" > /dev/null
 
-echo "create vid"
-vidurl=$(curl -sk -X POST -H "$auth" -H "Content-Type: application/json" -d "{\"originalImageFileId\":\"$fileid\",\"depthMapImageFileId\":\"$fileidmap\",\"animationParams\":{\"convergence\":0,\"animationLength\":3,\"phaseX\":0,\"phaseY\":0.25,\"phaseZ\":0.25,\"amplitudeX\":0.7875,\"amplitudeY\":0.15113350125944586,\"amplitudeZ\":1.2505499999999998},\"isPaid\":false}" "https://leiapixconverter-backend.leiapix.com/api/v2/generateMp4" |  jq '.downloadUrl' | tr -d '"')
+#echo "create vid"
+vidurl=$(curl -sk -X POST -H "$auth" -H "Content-Type: application/json" -d "{\"originalImageFileId\":\"$fileid\",\"depthMapImageFileId\":\"$fileidmap\",\"animationParams\":{\"convergence\":0,\"animationLength\":5,\"phaseX\":0,\"phaseY\":0.25,\"phaseZ\":0.25,\"amplitudeX\":0.7875,\"amplitudeY\":0.15113350125944586,\"amplitudeZ\":1.2505499999999998},\"isPaid\":false}" "https://leiapixconverter-backend.leiapix.com/api/v2/generateMp4" |  jq '.downloadUrl' | tr -d '"')
 
 #echo  $vidurl
-echo "download vid"
+#echo "download vid"
 
-curl  -sk $vidurl -o testvid.mp4
+curl  -sk $vidurl -o testvid$2.mp4
+#echo "delete map"
+
+rm testmap$2.jpg
+
 echo "done"
